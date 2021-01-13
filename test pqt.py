af91135dd0eab709f079e3af62ac6792a44796d7
a.bessonov <a.bessonov@orangesystem.ru>
2021-01-13 15:52:23 +0300

    create table get cell signal

--- test pqt.py
+++ test pqt.py
@@ -1,1 +1,20 @@
-print(1)
+# pyuic5 -x untitled.ui -o foo.py
+from foo import *
+
+def say_hello(a):
+ print("Button clicked, Hello!")
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+
+
+    ui.setupUi(Dialog)
+    # ui.tableWidget.clearContents()
+    ui.tableWidget.cellPressed.connect(lambda x:print(ui.tableWidget.cellClicked.row()))
+    ui.Press.clicked.connect(ui.tableWidget.clearContents)
+    Dialog.show()
+    sys.exit(app.exec_())
